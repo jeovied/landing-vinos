d770ad8cad2fc83b4dc14e29d34e74b32c86f434
jeovied <jonathan.e.oviedo@gmail.com>
2023-09-18 12:32:01 -0300

    footer & header created - html logo updated & name was changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import { CardList } from "./Components/CardList";
+import { Container } from "@mui/material";
+import { Footer, Header } from "./Components";
 
 function App() {
-
     return (
-        <>
-            <h1>Landing Vinos</h1>
-
+        <Container
+            maxWidth={false}
+            disableGutters
+            sx={{
+                display: "flex",
+                height: "100vh",
+                flexDirection: "column",
+                justifyContent: "space-between",
+            }}
+        >
+            <Header />
             <CardList />
-        </>
+            <Footer />
+        </Container>
     );
 }
 
