c469076161702f45fbcb56bbf3940080aba97c62
jeovied <jonathan.e.oviedo@gmail.com>
2023-09-19 01:11:23 -0300

    playsinline prop añadido en Video

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
                 </Typography>
             </Box>
             <Box component="main">
-                <Video src={bannerVideo} autoPlay muted loop />
+                <Video src={bannerVideo} autoPlay muted playsInline loop />
             </Box>
         </Container>
     );
