acfe45f3fe9a3881927806a3730ed81bdc065668
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-21 12:43:31 -0300

    Agrega data.js para los productos

---
+++ src/Components/Card/data.js
@@ -0,0 +1,62 @@
+export const cards = [
+  {
+    id: '01',
+    img: 'https://ucarecdn.com/dc4ed915-96ab-456a-9021-eb65b2e2d2c8/',
+    title: 'Lui District Blend de Parcelas Appellation Los Chacayes 2018',
+    description: 'De color rojo intenso y profundo con ribetes violáceos. La nariz se centra en la frescura, con aromas a moras y gro sellas combinadas con notas especiadas y hierbas aromáticas. En boca sus taninos inspiran suavidad y concentración al mismo tiempo. La acidez natural de Los Chacayes le da a este vino su elegancia y profundidad.'
+  },
+  {
+    id: '02',
+    img: 'https://ucarecdn.com/894c5e20-7b66-4bf9-af12-bc525f9ad0b8/',
+    title: 'Dedicado Gran Corte 2020',
+    description: 'Premiado por la revista internacional Decanter con 97 puntos. Dedicado Gran Corte es el vino que dio origen a la línea Dedicado. Elaborado únicamente en años con cosechas excepcionales. Se trata de un blend de tintas de las mejores uvas. Es un vino con gran intensidad aromática, destacándose notas a ciruelas, especias dulces y pimientos. De cuerpo robusto, es un vino complejo con taninos maduros y larga persistencia en boca.'
+  },
+  {
+    id: '03',
+    img: 'https://ucarecdn.com/9c5030e0-6be7-4720-b90a-25d897a08071/',
+    title: 'Hermandad Winemaker Series Cabernet Franc 2019',
+    description: 'Es un tinto de color rubí intenso y brillante que aromáticamente es pura complejidad. Hay un buena presencia del roble (pasa 14 meses en barricas) que suma estilo clásico y se funde con los tonos frutales y especiados como clavo de olor, canela, ahumados, cedro y tabaco con tonos de cerezas negras, casis y arándanos con fondo herbal, algo mentolado. En boca es amplio y brioso, voluptuoso y envolvente, fresco con taninos firmes y final prolongado.'
+  },
+  {
+    id: '04',
+    img: 'https://ucarecdn.com/be9bf6bd-846c-4b7b-8fb7-911d95531ab3/',
+    title: 'Los Helechos Designados Cabernet Franc',
+    description: 'Es la línea de vinos top de Fecovita y estre sus etiquetas ofrece Designado Cabernet Franc, vino que elaboran con uvas de Vista Flores, Valle de Uco. Criado por 16 meses en roble afrece una exquisita expresión varietal con el sello de los vinos de montaña. Es de color púrpura intenso, ofrece un trazo de hierbas y perfil de fruta negra con tonos del roble primer término. Paladar carnoso y de volumen medio, con taninos firmes y la fruta negra confitada nuevamente presente. Final grato con dejos mentolado.'
+  },
+  {
+    id: '05',
+    img: 'https://ucarecdn.com/0bf9a9e8-573d-4863-81d4-6df042d9d821/',
+    title: 'Victorio Altieri Gran Reserva Malbec 2020',
+    description: 'Excelente Malbec, rubi intenso con ribetes púrpuras, no muy expresivo en nariz, pero con aromas agradables a Mora y ciruela madura, notas a tierra, hierbas y madera con dejos a chocolate. En boca tiene cuerpo medio, es elegante y redondo. Acidez media y taninos amables, final frutado y persistente'
+  },
+  {
+    id: '06',
+    img: 'https://ucarecdn.com/ccad5492-d765-4dd0-8006-7f58adfbef8c/',
+    title: 'Ameri Malbec 2020',
+    description: 'Cosecha manual a comienzos de Abril. Cuidadosa selección manual de los racimos y las uvas. Fermentación con levaduras seleccionadas a máxima temperatura entre 25° C y 28° C por 14 días y 20 días de maceración en barricas de roble. La temperatura de la fermentación se mantiene baja, permitiendo extraer intensos aromas. 100% maloláctica. Crianza en roble francés por 12 meses.'
+  },
+  {
+    id: '07',
+    img: 'https://ucarecdn.com/fad094a2-3a8d-45c1-ad81-2002a9fd1f85/',
+    title: 'Gran Vin Blend 2018',
+    description: 'Cosecha manual en cajas de 12 kgs. Tras una doble selección de racimos y granos, los tanques de acero inoxidable se llenan por gravedad para dar incio al proceso de maceración. Este proceso consta de tres etapas : maceración fría (7 días a 8ºC), maceración fermentativa (7 a 10 días a 28ºC) y maceración post fermentativa hasta completar los 45 a 60 días que dura el proceso total. Fermentación Maloláctica 100% en barricas nuevas. Crianza 100% en barricas de roble francés durante 18 meses.'
+  },
+  {
+    id: '08',
+    img: 'https://ucarecdn.com/77d65441-e9a8-4013-8bca-08fdfaa9752b/',
+    title: 'Trinitá Gran Reserva Blend 2020',
+    description: 'Trinita presenta un intenso color rojo granate con matices violaceos. En nariz recuerda a frutas como frambuesa, cereza y ciruela, caracteristicas de los varietales que conforman este blend. Tambien se perciben aromas especiados como pimienta negra y clavo de olor y aromas obtenidos durante su crianza en barricas como chocolate y vainilla. En boca se siente equilibrado de buen cuerpo y persistente.'
+  },
+  {
+    id: '09',
+    img: 'https://ucarecdn.com/9d7447bc-dacf-4932-8c58-4f940615dcee/',
+    title: 'Salentein Single Vineyard El Tomillo Malbec 2019',
+    description: 'El intenso aroma del tomillo salvaje que crece abundantemente en la viña dio origen al nombre "El Tomillo". Este es el lugar de nacimiento de uno de los vinos de Malbec más distinguidos de Salentein.Este vino destaca por la complejidad de sus aromas a frutas rojas y especias. En boca es elegante con taninos aterciopelados y finos, notas minerales y especiadas sostenidas por una acidez refrescante y un agradable final persistente.'
+  },
+  {
+    id: '10',
+    img: 'https://ucarecdn.com/4bac66ea-b5c2-46f6-860f-d40f62c0a694/',
+    title: 'Diamandes de Uco Malbec 2019',
+    description: 'DiamAndes de Uco Malbec es un Varietal 100% Malbec que revela toda la redondez, la riqueza y la intensidad del Malbec, cepaje emblemático de Argentina. Además de su fuerte identidad varietal, este vino se caracteriza por la gran elegancia que le confiere la tipicidad del viñedo de bodega DiamAndes, situado a 1.100 metros de altitud. Su particular clima de altitud, en el que alternan noches frías con días cálidos y soleados, le permite mantener una buena acidez que aportan frescor y equilibrio a los vinos.'
+  },
+];
